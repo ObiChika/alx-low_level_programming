7eaae86a4cdf703c1ca8a77d2a03c1ce4ef6a3e7
ObiChika <amandachika85@gmail.com>
2023-07-19 13:11:48 -0700

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,64 @@
-#include "3-calc.h"
-#include <stddef.h>
+/**
+ * op_add - Adds two integers.
+ *
+ * @a: The first input integer.
+ * @b: The second input integer.
+ *
+ * Return: The sum of @a and @b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
 /**
- * get_op_func - Get the function pointer for a given operator symbol.
+ * op_sub - Subtracts @b from @a.
  *
- * @s: A char pointer pointing to an operator symbol from the program argument.
+ * @a: The first input integer.
+ * @b: The second input integer.
  *
- * Return: One of the op to perform calculations, or NULL if no match is found.
+ * Return: The difference between @a and @b.
  */
-int (*get_op_func(char *s))(int, int)
+int op_sub(int a, int b)
 {
-	/* Array of struct op_t to store operators */
-	op_t ops[] = {
-		{"+", op_add}, /* Operator: "+", Function: op_add */
-		{"-", op_sub}, /* Operator: "-", Function: op_sub */
-		{"*", op_mul}, /* Operator: "*", Function: op_mul */
-		{"/", op_div}, /* Operator: "/", Function: op_div */
-		{"%", op_mod}, /* Operator: "%", Function: op_mod */
-		{NULL, NULL}	/* NULL terminator for the array, marks the end of operators */
-	};
+	return (a - b);
+}
 
-	int i = 0;
+/**
+ * op_mul - Multiplies two integers.
+ *
+ * @a: The first input integer.
+ * @b: The second input integer.
+ *
+ * Return: The product of @a and @b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
-	while (i < 5)
-	{
-		if (*s == *ops[i].op)
-			return (ops[i].f);
-		i++;
-	}
+/**
+ * op_div - Divides @a by @b.
+ *
+ * @a: The dividend (input 1).
+ * @b: The divisor (input 2).
+ *
+ * Return: The result of the division of @a by @b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
 
-	return (NULL); /* Return NULL if no match is found */
+/**
+ * op_mod - Gets the remainder of the division between @a and @b.
+ *
+ * @a: The dividend (input 1).
+ * @b: The divisor (input 2).
+ *
+ * Return: The remainder of the division of @a by @b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }

